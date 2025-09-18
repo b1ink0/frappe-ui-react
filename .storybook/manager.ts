4440aac76f555b5658c5a6d49c43ea776969355c
sayedtaqui <sayedwp@gmail.com>
2025-09-18 13:42:34 +0530

    Update name

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,7 +4,7 @@ import { create } from 'storybook/theming';
 addons.setConfig({
   theme: create({
     base: 'light',
-    brandTitle: 'Frappe UI React',
+    brandTitle: 'frappe-ui-react',
     brandUrl: undefined, // disables link on the title
     brandImage: undefined
   }),
