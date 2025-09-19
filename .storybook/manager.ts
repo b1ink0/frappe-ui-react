7029cbbf3a253b3127e65a42ada8bf7868b5a6fb
sayedtaqui <sayedwp@gmail.com>
2025-09-19 16:10:34 +0400

    Add version number

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -4,7 +4,7 @@ import { create } from 'storybook/theming';
 addons.setConfig({
   theme: create({
     base: 'light',
-    brandTitle: 'frappe-ui-react',
+    brandTitle: '<span id="fur-title">frappe-ui-react</span> <span id="fur-version">(v1.0.0-beta.0)</span>', // update version as per package.json
     brandUrl: undefined, // disables link on the title
     brandImage: undefined
   }),
