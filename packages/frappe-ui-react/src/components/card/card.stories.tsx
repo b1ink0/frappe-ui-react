aa95603cbe2eae84d0a891c5c98241068005347d
Mayank Rana <mayankranax1@gmail.com>
2025-10-08 11:16:31 +0530

    Add Card component with header, title, description, content, and footer

---
+++ packages/frappe-ui-react/src/components/card/card.stories.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "./card";
+import { Button } from "../button";
+import { MemoryRouter } from "react-router";
+
+export default {
+  title: "Components/Card",
+  component: Card,
+  tags: ["autodocs"],
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Card component for grouping related content. Includes header, title, description, content, and footer.",
+      },
+    },
+  },
+};
+
+export const Basic = () => (
+  <MemoryRouter>
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>This is a card description.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>This is the main content of the card.</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Action</Button>
+      </CardFooter>
+    </Card>
+  </MemoryRouter>
+);
